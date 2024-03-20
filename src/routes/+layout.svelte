b5d40ba026602e6beb9cc591bc0d54ddb7870772
akongstad <kongstad@outlook.com>
2024-03-20 13:40:06 +0100

    refactor nav bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { toggleMode } from 'mode-watcher';
+import { DiscordLogo, GithubLogo, LinkedinLogo } from 'svelte-radix';
+import Sun from 'svelte-radix/Sun.svelte';
+import Moon from 'svelte-radix/Moon.svelte';
+import { Separator } from '$lib/components/ui/separator';
+import { Button } from '$lib/components/ui/button';
+import * as Avatar from '$lib/components/ui/avatar';
+import { Skeleton } from '$lib/components/ui/skeleton';
+
+let page = 'home';
+
+</script>
+
+
+<!--Navbar-->
+<nav class="flex justify-between bg-opacity-30 pt-5 px-8 pb-3">
+	<div>
+		<a href="/" class="inline-flex hover:underline space-x-2">
+			<Avatar.Root class="size-8">
+				<Avatar.Image src="https://github.com/Akongstad.png" alt="Andreas Kongstad" />
+				<Avatar.Fallback>
+					<Skeleton class="size-8 rounded-full" />
+				</Avatar.Fallback>
+			</Avatar.Root>
+			<div class="text-lg font-semibold pt-1">andreas/kongstad</div>
+		</a>
+	</div>
+	<a href="/projects" class="text-bsm text-muted-foreground hover:text-accent-foreground">
+		<div class="text-lg font-light pt-1">projects</div>
+	</a>
+	<a href="/blog" class="text-bsm text-muted-foreground hover:text-accent-foreground cursor-not-allowed">
+		<div class="text-lg font-light pt-1">blog</div>
+	</a>
+	<div class="px-60"></div>
+	<!--socials-->
+	<div>
+		<Button variant="ghost" size="icon">
+			<GithubLogo class="h-6 w-6" />
+		</Button>
+		<Button variant="ghost" size="icon">
+			<LinkedinLogo class="h-6 w-6" />
+		</Button>
+		<Button variant="ghost" size="icon">
+			<DiscordLogo class="h-6 w-6" />
+		</Button>
+		<Button on:click={toggleMode} variant="ghost" size="icon">
+			<Sun class="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<Moon class="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+		</Button>
+	</div>
+</nav>
+<Separator class="my-1" />
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,8 @@
 <script lang="ts">
 	import '../app.pcss';
-	import { Separator } from '$lib/components/ui/separator';
-	import * as Avatar from '$lib/components/ui/avatar';
-	import Sun from "svelte-radix/Sun.svelte";
-	import Moon from "svelte-radix/Moon.svelte";
-	import {LinkedinLogo, GithubLogo, DiscordLogo} from 'svelte-radix';
-	import { Button } from '$lib/components/ui/button/index.js';
-	import { toggleMode } from "mode-watcher";
 </script>
 
-<!--Navbar-->
-<div class="flex justify-between">
-	<ul class="flex pl-10 pt-5">
-		<li class="mr-6 px-1">
-			<a href="/" class="inline-flex hover:underline space-x-2">
-				<Avatar.Root class="size-8">
-					<Avatar.Image src="https://github.com/Akongstad.png" alt="Andreas Kongstad" />
-					<Avatar.Fallback>AK</Avatar.Fallback>
-				</Avatar.Root>
-				<div class="text-base font-semibold pt-1">andreas/kongstad</div>
-			</a>
-		</li>
-		<li class="mr-6 px-1">
-			<a href="/projects" class="text-bsm text-muted-foreground hover:text-accent-foreground">
-				<div class="text-base font-light pt-1">projects</div>
-			</a>
-		<li class="mr-6 px-1">
-			<a href="/blog" class="text-bsm text-muted-foreground hover:text-accent-foreground cursor-not-allowed">
-				<div class="text-base font-light pt-1">blog</div>
-			</a>
-		</li>
-	</ul>
-	<!--socials-->
-	<ul class="flex pl-10 pt-5 pr-3">
-		<li >
-			<Button variant="ghost" size="icon" >
-				<GithubLogo class="h-4 w-4" />
-			</Button>
-			<Button variant="ghost" size="icon" >
-				<LinkedinLogo class="h-4 w-4" />
-			</Button>
-			<Button variant="ghost" size="icon" >
-				<DiscordLogo class="h-4 w-4" />
-			</Button>
-			<Button on:click={toggleMode} variant="ghost" size="icon">
-				<Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-				<Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-			</Button>
-		</li>
-	</ul>
-</div>
 
-
-<Separator class="my-1" />
 <div class="text-center">
 	<header>
 		<h1 class="p-5 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl first:mt-0">

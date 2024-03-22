2508cf039eac63ea597f20492eb5ad4ad4e9df17
akongstad <kongstad@outlook.com>
2024-03-22 17:04:45 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -29,8 +29,8 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			//base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			base: process.argv.includes('dev') ? '' : `/andreaskongstad.dev`
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			//base: process.argv.includes('dev') ? '' : `/andreaskongstad.dev`
 		}
 	}
 };
